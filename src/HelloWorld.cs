e826337bd772bf242463cc0f3cb6a9300b35d742
bryanjhc <bryanchew22@gmail.com>
2024-01-10 15:09:20 +0800

    Code your contract 1.3.

--- src/HelloWorld.cs
+++ src/HelloWorld.cs
@@ -1,4 +1,5 @@
 using AElf.Sdk.CSharp;
+using AElf.Types;
 using Google.Protobuf.WellKnownTypes;
 
 namespace AElf.Contracts.HelloWorld
@@ -6,29 +7,38 @@ namespace AElf.Contracts.HelloWorld
     // Contract class must inherit the base class generated from the proto file
     public class HelloWorld : HelloWorldContainer.HelloWorldBase
     {
-        // A method that modifies the contract state
-        public override Empty Update(StringValue input)
+        // adding this line is for preparing the contract deployment later,
+        // to differentiate each person's contract.
+        // This is because our testnet does not allow the deployment of two identical contracts.
+        const string author = "your name";
+
+        public override Empty Initialize(Empty input)
         {
-            // Set the message value in the contract state
-            State.Message.Value = input.Value;
-            // Emit an event to notify listeners about something happened during the execution of this method
-            Context.Fire(new UpdatedMessage
-            {
-                Value = input.Value
-            });
+            Assert(!State.Initialized.Value, "already initialized");
+            State.RandomNumberContract.Value =
+                Context.GetContractAddressByName(SmartContractConstants.ConsensusContractSystemName);
             return new Empty();
         }
 
-        // A method that read the contract state
-        public override StringValue Read(Empty input)
+        public override Character CreateCharacter(Empty input)
         {
-            // Retrieve the value from the state
-            var value = State.Message.Value;
-            // Wrap the value in the return type
-            return new StringValue
+            var randomBytes = State.RandomNumberContract.GetRandomBytes
+                .Call(new Int64Value { Value = Context.CurrentHeight - 1 }.ToBytesValue()).Value.ToByteArray();
+            var hash = HashHelper.ComputeFrom(Context.Sender).Value.ToByteArray();
+
+            var character = new Character
             {
-                Value = value
+                Health = 60 + (randomBytes[2] ^ hash[2]) % 41, // Health is 60 ~ 100
+                Strength = 40 + (randomBytes[3] ^ hash[3]) % 61, // Strength is 40 ~ 100
+                Speed = 100 + (randomBytes[4] ^ hash[4]) % 101 // Speed is 100 ~ 200
             };
+            State.Characters[Context.Sender] = character;
+            return character;
+        }
+
+        public override Character GetMyCharacter(Address input)
+        {
+            return State.Characters[input] ?? new Character();
         }
     }
     
